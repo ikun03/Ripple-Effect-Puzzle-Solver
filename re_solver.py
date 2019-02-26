576fddd732780fd3960d19dca6b45b031fa435b2
Kunal <ikunal03@gmail.com>
2019-02-26 02:35:58 -0500

    Attempted to process graph

--- re_solver.py
+++ re_solver.py
@@ -5,7 +5,10 @@ class PuzzleCell:
         self.value = value
         self.row = row
         self.column = column
-        self.id = row + column
+        self.id = str(row) + "," + str(column)
+
+    def __str__(self):
+        return str(self.row) + "," + str(self.column)
 
 
 def inAddedCells(addedCellsDict, rowCounter, columnCounter):
@@ -15,31 +18,106 @@ def inAddedCells(addedCellsDict, rowCounter, columnCounter):
         return False
 
 
-def getAllCellsOfRegion(lineIndex, charIndex, lineArray, matrixOfCells):
-    pass
+def getAllRegionCells(rowCounter, columnCounter, lineArray, lineIndex, charIndex, nodeList):
+    # For checking the bottom cell
+    lineIndexCounter = lineIndex + 1
+    while len(lineArray[lineIndexCounter]) < (charIndex + 1) or lineArray[lineIndexCounter][charIndex] == " ":
+        lineIndexCounter += 1
+    if lineArray[lineIndexCounter][charIndex] != "-":
+        if lineArray[lineIndexCounter][charIndex] == ".":
+            isPresent = False
+            for node in nodeList:
+                if node.id == (str(rowCounter + 1) + "," + str(columnCounter)):
+                    isPresent = True
+                    break
+            if not isPresent:
+                nodeList.append(PuzzleCell(lineArray[lineIndexCounter][charIndex], rowCounter + 1, columnCounter))
+                nodeList = getAllRegionCells(rowCounter + 1, columnCounter, lineArray, lineIndexCounter, charIndex,
+                                             nodeList)
+
+    # For checking the top cell
+    lineIndexCounter = lineIndex - 1
+    while len(lineArray[lineIndexCounter]) < (charIndex +1) or lineArray[lineIndexCounter][charIndex] == " ":
+        lineIndexCounter -= 1
+    if lineArray[lineIndexCounter][charIndex] != "-":
+        if lineArray[lineIndexCounter][charIndex] == ".":
+            isPresent = False
+            for node in nodeList:
+                if node.id == (str(rowCounter - 1) + "," + str(columnCounter)):
+                    isPresent = True
+                    break
+            if not isPresent:
+                nodeList.append(PuzzleCell(lineArray[lineIndexCounter][charIndex], rowCounter - 1, columnCounter))
+                nodeList = getAllRegionCells(rowCounter - 1, columnCounter, lineArray, lineIndexCounter, charIndex,
+                                             nodeList)
+
+    # For checking right cell
+    charIndexCounter = charIndex + 1
+    while lineArray[lineIndex][charIndexCounter] == " ":
+        charIndexCounter += 1
+    if lineArray[lineIndex][charIndexCounter] != "|":
+        if lineArray[lineIndex][charIndexCounter] == ".":
+            isPresent = False
+            for node in nodeList:
+                if node.id == (str(rowCounter) + "," + str(columnCounter + 1)):
+                    isPresent = True
+                    break
+            if not isPresent:
+                nodeList.append(PuzzleCell(lineArray[lineIndex][charIndexCounter], rowCounter, columnCounter + 1))
+                nodeList = getAllRegionCells(rowCounter, columnCounter + 1, lineArray, lineIndex, charIndexCounter,
+                                             nodeList)
+
+    # For checking left cell
+    charIndexCounter = charIndex - 1
+    while lineArray[lineIndex][charIndexCounter] == " ":
+        charIndexCounter -= 1
+    if lineArray[lineIndex][charIndexCounter] != "|":
+        if lineArray[lineIndex][charIndexCounter] == ".":
+            isPresent = False
+            for node in nodeList:
+                if node.id == (str(rowCounter) + "," + str(columnCounter - 1)):
+                    isPresent = True
+                    break
+            if not isPresent:
+                nodeList.append(PuzzleCell(lineArray[lineIndex][charIndexCounter], rowCounter, columnCounter - 1))
+                nodeList = getAllRegionCells(rowCounter, columnCounter + 1, lineArray, lineIndex, charIndexCounter,
+                                             nodeList)
+
+    return nodeList
 
 
 def processPuzzleArray(puzzleHeight, puzzleWidth, lineArray):
     rowCounter = 0
-    columnCounter = 0
     listOfRegions = []
     addedCellsDict = {}
     regionCounter = 0
 
     matrixOfCells = []
     for length in range(puzzleHeight):
-        matrixOfCells.append([])
+        list = []
+        for width in range(puzzleWidth):
+            list.append([])
+        matrixOfCells.append(list)
 
     for lineIndex in range(1, len(lineArray) - 1):
         columnCounter = 0
-        for charIndex in lineArray[lineIndex]:
+        for charIndex in range(1, len(lineArray[lineIndex]) - 1):
             charToCheck = lineArray[lineIndex][charIndex]
-            if charToCheck == "." or charToCheck > 0:
+            if charToCheck not in ['|', '-', ' '] and (charToCheck == "." or int(charToCheck) > 0):
+                nodeList = []
                 if not inAddedCells(addedCellsDict, rowCounter, columnCounter):
-                    puzzleCell = PuzzleCell(rowCounter, columnCounter, int(charToCheck))
-                    addedCellsDict[rowCounter + columnCounter] = puzzleCell
-                    matrixOfCells.append(puzzleCell)
-                    regionList=getAllCellsOfRegion(lineIndex,charIndex,lineArray,matrixOfCells)
+                    puzzleCell = PuzzleCell(charToCheck, rowCounter, columnCounter)
+                    nodeList = [puzzleCell]
+                    nodeList = getAllRegionCells(rowCounter, columnCounter, lineArray, lineIndex, charIndex, nodeList)
+                    for node in nodeList:
+                        addedCellsDict[node.row + node.column] = node
+                        matrixOfCells[node.row][node.column] = node
+                listOfRegions.append(nodeList)
+                regionCounter += 1
+                columnCounter += 1
+        if "." in lineArray[lineIndex]:
+            rowCounter += 1
+    print(listOfRegions)
 
 
 def main():
